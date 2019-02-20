a80479087c1ee6bcf8fbaa1c5041f3e37547072e
Rahul Behera <rbehera@gmail.com>
2019-02-20 15:50:37 -0600

    ok call some native os stuff

--- wire/src/macosMain/kotlin/sample/SampleMacos.kt
+++ wire/src/macosMain/kotlin/sample/SampleMacos.kt
@@ -1,9 +1,11 @@
 package sample
 
+import platform.Foundation.NSProcessInfo
+
 actual class Sample {
     actual fun checkMe() = 7
 }
 
 actual object Platform {
-    actual val name: String = "Native"
+    actual val name: String = NSProcessInfo.processInfo().operatingSystemName()
 }
