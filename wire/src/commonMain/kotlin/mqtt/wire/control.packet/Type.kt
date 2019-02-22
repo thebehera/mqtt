8fc7f8634f48033e9455113ccedab50d74919f76
Rahul Behera <rbehera@gmail.com>
2019-02-22 00:22:37 -0600

    small progress on the control packet stuff. alot of it is garbage

--- wire/src/commonMain/kotlin/mqtt/wire/control.packet/Type.kt
+++ wire/src/commonMain/kotlin/mqtt/wire/control.packet/Type.kt
@@ -6,7 +6,6 @@ import mqtt.wire.control.packet.DirectionOfFlow.FORBIDDEN
 /**
  * <table><tr><td><b>Bit</b><td><b>7</b><td><b>6</b><td><b>5</b><td><b>4</b><td><b>3</b><td><b>2</b><td><b>1</b><td><b>0</b><tr><td>byte 1<td>MQTT Control Packet type<td>Flags specific to each MQTT Control Packet type<tr><td>byte 2<td>Remaining Length</table>
  */
-@Suppress("UNUSED_PARAMETER")
 enum class ControlPacketType(value: Byte, direction: DirectionOfFlow) {
     RESERVED(0, FORBIDDEN),
     /**
