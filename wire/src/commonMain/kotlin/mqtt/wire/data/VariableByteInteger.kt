a6b06c027600315fc621f718087963058d8701cd
Rahul Behera <rbehera@gmail.com>
2019-02-22 19:11:43 -0600

    add variable byte integer tests

--- wire/src/commonMain/kotlin/mqtt/wire/data/VariableByteInteger.kt
+++ wire/src/commonMain/kotlin/mqtt/wire/data/VariableByteInteger.kt
@@ -4,11 +4,15 @@ package mqtt.wire.data
 
 import kotlinx.io.core.buildPacket
 import kotlinx.io.core.readBytes
+import kotlinx.io.errors.IOException
+import kotlin.experimental.and
 import kotlin.experimental.or
 
 
-fun Int.encodeVariableByteInteger(): ByteArray {
-    validateVariableByteInt(this)
+private val VARIABLE_BYTE_INT_MAX = 268435455
+
+fun Int.encodeVariableByteInteger(bypassValidation: Boolean = false): ByteArray {
+    if (!bypassValidation) validateVariableByteInt(this)
     var numBytes = 0
     var no = toLong()
 
@@ -23,17 +27,35 @@ fun Int.encodeVariableByteInteger(): ByteArray {
             numBytes++
         } while (no > 0 && numBytes < 4)
     }
-
     return packet.readBytes()
 }
 
+fun ByteArray.decodeVariableByteInteger(): Int {
+    var digit: Byte
+    var value = 0
+    var multiplier = 1
+    var count = 0
+    var byteArrayIndex = 0
+    try {
+        do {
+            digit = get(byteArrayIndex++)
+            count++
+            value += (digit and 0x7F) * multiplier
+            multiplier *= 128
+        } while ((digit and 0x80.toByte()).toInt() != 0)
+    } catch (e: IndexOutOfBoundsException) {
+        throw MqttMalformedInvalidVariableByteInteger(value)
+    }
+    return value
+}
 
-private val VARIABLE_BYTE_INT_MAX = 268435455
 fun validateVariableByteInt(value: Int) {
     if (value in 0..VARIABLE_BYTE_INT_MAX) {
         return
     } else {
-        throw IllegalArgumentException("This property must be a number between 0 and $VARIABLE_BYTE_INT_MAX")
+        throw MqttMalformedInvalidVariableByteInteger(value)
     }
-
 }
+
+class MqttMalformedInvalidVariableByteInteger(val value: Int) : IOException("Malformed Variable Byte Integer: This " +
+        "property must be a number between 0 and %VARIABLE_BYTE_INT_MAX . Read value was: $value")
