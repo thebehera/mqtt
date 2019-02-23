eecd5cdfe66f5f4e741480dcdc84ab96a8ef6f93
Rahul Behera <rbehera@gmail.com>
2019-02-22 20:13:30 -0600

    add one more test

--- wire/src/commonTest/kotlin/mqtt/wire/data/StringTests.kt
+++ wire/src/commonTest/kotlin/mqtt/wire/data/StringTests.kt
@@ -8,6 +8,17 @@ class StringTests {
     @Test
     fun invalidMqttString() = assertFalse("abc\u0001def".validateMqttUTF8String())
 
+
     @Test
-    fun validMqttString() = assertTrue("abcdef".validateMqttUTF8String())
+    fun validMqttString() = assertTrue("abc\u002Fdef".validateMqttUTF8String())
+
+    @Test
+    fun invalidMqttStringPoint2() = assertFalse("abc\u007fdef".validateMqttUTF8String())
+
+    @Test
+    fun validMqttStringBasic() = assertTrue("abcdef".validateMqttUTF8String())
+
+    @Test
+    fun nonCharacter() = assertFalse("abc\u0fffdef".validateMqttUTF8String())
+
 }
