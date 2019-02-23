3404fb8a7a0eb27fdbc270f8bcfe2b6760eea0fe
Rahul Behera <rbehera@gmail.com>
2019-02-22 19:11:43 -0600

    add variable byte integer tests

---
+++ wire/src/commonTest/kotlin/mqtt/wire/data/VariableByteIntegerTests.kt
@@ -0,0 +1,26 @@
+package mqtt.wire.data
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.fail
+
+class VariableByteIntegerTests {
+
+    @Test
+    fun encodeAndDecode() {
+        val numberToEncode = 268435442
+        val encodedNumber = numberToEncode.encodeVariableByteInteger()
+        val decoded = encodedNumber.decodeVariableByteInteger()
+        assertEquals(numberToEncode, decoded)
+    }
+
+    @Test
+    fun oversize() {
+        try {
+            val encodedByteArray = Int.MAX_VALUE.encodeVariableByteInteger(true)
+            encodedByteArray.decodeVariableByteInteger()
+            fail("Should of have hit an exception")
+        } catch (e: MqttMalformedInvalidVariableByteInteger) {
+        }
+    }
+}
