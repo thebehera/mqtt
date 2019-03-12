b9aa8024467dd01fbbb720877625de19c9f54c7f
Rahul Behera <rbehera@gmail.com>
2019-03-12 17:12:17 -0500

    speed up and fix the checking if the char is in range. definatly speed up the tests

--- wire/src/commonMain/kotlin/mqtt/wire/data/String.kt
+++ wire/src/commonMain/kotlin/mqtt/wire/data/String.kt
@@ -9,38 +9,26 @@ fun String.validateMqttUTF8String(): Boolean {
     if (length > 65_535) {
         return false
     }
-    val index = indexOfAny(controlCharacters + shouldNotIncludeCharacters + unicodePrivateUseCharacters)
-    return index == -1
+    for (c in this) {
+        if (c in controlCharactersRange) return false
+        if (c in shouldNotIncludeCharRange1) return false
+        if (c in shouldNotIncludeCharRange2) return false
+        if (c in privateUseCharRange) return false
+    }
+    return true
 }
 
 class InvalidMqttUtf8StringMalformedPacketException(msg: String, indexOfError: Int, originalString: String)
     :MalformedPacketException("Fails to match MQTT Spec for a UTF-8 String. Error:($msg) at index $indexOfError of $originalString")
 
-private val controlCharacters by lazy {
-    val chars = '\uD800'..'\uDFFF'
-    val list = chars.toMutableSet()
-    list += '\u0000'
-    list.toCharArray()
-}
-
-private val shouldNotIncludeCharacters by lazy {
-    val u0001To001F = '\u0001'..'\u001F'
-    val u0001To001FSet = u0001To001F.toSet()
-    val u007FTo009F = '\u007F'..'\u009F'
-    val u007FTo009FSet = u007FTo009F.toSet()
-    val shouldNotIncludeCharacters = u0001To001FSet + u007FTo009FSet
-    shouldNotIncludeCharacters.toCharArray()
-}
-
+private val controlCharactersRange by lazy { '\uD800'..'\uDFFF' }
+private val shouldNotIncludeCharRange1 by lazy { '\u0001'..'\u001F' }
+private val shouldNotIncludeCharRange2 by lazy { '\u007F'..'\u009F' }
 /**
  * Cannot add planes 15 or 16 as it does not compile into a 'char' in kotlin
  * http://www.unicode.org/faq/private_use.html#pua2
  */
-private val unicodePrivateUseCharacters by lazy {
-    val mainRange = '\uE000'..'\uF8FF'
-    val mainRangeSet = mainRange.toSet()
-    mainRangeSet.toCharArray()
-}
+private val privateUseCharRange by lazy { '\uE000'..'\uF8FF' }
 
 inline class MqttUtf8String(private val value: String) {
     fun getValueOrThrow(includeWarnings: Boolean = true): String {
@@ -60,22 +48,24 @@ inline class MqttUtf8String(private val value: String) {
             if (value.length > 65_535) {
                 return InvalidMqttUtf8StringMalformedPacketException("MQTT UTF-8 String too large", 65_535, value.substring(0, 65_535))
             }
-            val index = value.indexOfAny(controlCharacters)
-            if (index > -1) {
-                return InvalidMqttUtf8StringMalformedPacketException("Invalid Control Character", index, value)
+            value.forEachIndexed { index, c ->
+                if (c == '\u0000')
+                    return InvalidMqttUtf8StringMalformedPacketException("Invalid Control Character null \\u0000", index, value)
+                if (c in controlCharactersRange)
+                    return InvalidMqttUtf8StringMalformedPacketException("Invalid Control Character (\\uD800..\\uDFFF)", index, value)
             }
             return null
     }
 
     private val warning: InvalidMqttUtf8StringMalformedPacketException?
         get() {
-            val index = value.indexOfAny(shouldNotIncludeCharacters)
-            if (index > -1) {
-                return InvalidMqttUtf8StringMalformedPacketException("Disallowed Unicode Code Point", index, value)
-            }
-            val indexUnicode = value.indexOfAny(unicodePrivateUseCharacters)
-            if (indexUnicode > -1) {
-                return InvalidMqttUtf8StringMalformedPacketException("Unicode private use character", index, value)
+            value.forEachIndexed { index, c ->
+                if (c in shouldNotIncludeCharRange1)
+                    return InvalidMqttUtf8StringMalformedPacketException("Invalid Character in range (\\u0001..\\u001F)", index, value)
+                if (c in shouldNotIncludeCharRange2)
+                    return InvalidMqttUtf8StringMalformedPacketException("Invalid Character in range (\\u007F..\\u009F)", index, value)
+                if (c in privateUseCharRange)
+                    return InvalidMqttUtf8StringMalformedPacketException("Invalid Character in range (\\uE000..\\uF8FF)", index, value)
             }
             return null
         }
