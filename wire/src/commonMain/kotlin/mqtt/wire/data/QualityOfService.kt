07b438506e2a52d32d632a332be8719b3ab33279
Rahul Behera <rbehera@gmail.com>
2019-03-13 12:38:39 -0500

    remove the useless qos function

--- wire/src/commonMain/kotlin/mqtt/wire/data/QualityOfService.kt
+++ wire/src/commonMain/kotlin/mqtt/wire/data/QualityOfService.kt
@@ -6,15 +6,6 @@ enum class QualityOfService(val integerValue: Byte) {
     AT_MOST_ONCE(0),
     AT_LEAST_ONCE(1),
     EXACTLY_ONCE(2);
-
-    fun toBitInformation(): Pair<Boolean, Boolean> {
-        return when (this) {
-            AT_MOST_ONCE -> Pair(false, second = false) // 00
-            AT_LEAST_ONCE -> Pair(first = false, second = true) // 01
-            EXACTLY_ONCE -> Pair(first = true, second = false) // 11
-        }
-    }
-
     fun isGreaterThan(otherQos: QualityOfService) = integerValue > otherQos.integerValue
     companion object {
         fun fromBooleans(bit2: Boolean, bit1: Boolean) :QualityOfService {
