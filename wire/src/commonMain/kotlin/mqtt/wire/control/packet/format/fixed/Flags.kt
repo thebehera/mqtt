07ef291c4d57567872e5b7bc4a871cd7a4b5d208
Rahul Behera <rbehera@gmail.com>
2019-03-13 12:38:00 -0500

    remove the useless toByte function

--- wire/src/commonMain/kotlin/mqtt/wire/control/packet/format/fixed/Flags.kt
+++ wire/src/commonMain/kotlin/mqtt/wire/control/packet/format/fixed/Flags.kt
@@ -7,20 +7,3 @@ package mqtt.wire.control.packet.format.fixed
  * get the value at an index as a boolean
  */
 fun UByte.get(index: Int) = this.toInt().and(0b01.shl(index)) != 0
-
-/**
- * Bits in a byte are labelled 7 to 0. Bit number 7 is the most significant bit, the least significant bit is assigned
- * bit number 0.
- */
-fun BooleanArray.toByte(): Byte {
-    if (size > 8) {
-        throw IllegalArgumentException("Cannot pack information into a byte. Boolean Array too large")
-    }
-    var result = 0
-    forEachIndexed { index, it ->
-        if (it) {
-            result = result or (1 shl index)
-        }
-    }
-    return result.toByte()
-}
