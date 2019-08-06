2ece7735f02431d57523ce75f8be36692d74bd50
Rahul Behera <rbehera@gmail.com>
2019-08-05 23:34:19 -0700

    ok cool, got some kind of andorid client connected to a server!

--- android_app_sample/src/main/java/mqtt/android_app/MainActivity.kt
+++ android_app_sample/src/main/java/mqtt/android_app/MainActivity.kt
@@ -2,7 +2,6 @@ package mqtt.android_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
 import mqtt.client.connection.ConnectionParameters
 import mqtt.client.viewmodel.SingleConnectionMqttClient
@@ -12,8 +11,6 @@ class MainActivity : AppCompatActivity() {
     val client by lazy { ViewModelProviders.of(this).get(SingleConnectionMqttClient::class.java) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fragmentActivity = this as FragmentActivity
-
         client.connectAsync(
             ConnectionParameters(
                 "192.168.1.98", 60000, false, ConnectionRequest("yoloswag")
