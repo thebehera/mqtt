ff78d693731cadf71bc08bfb063898f6622a3cd4
Rahul Behera <rbehera@gmail.com>
2019-12-08 19:07:44 -0800

    remove stacktrace

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $TRAVIS_OS_NAME = 'linux' ]; then
-  ./gradlew check build allTests --stacktrace --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
+  ./gradlew check build allTests --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 else
-  ./gradlew check build -x lint $LINT allTests --stacktrace --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
+  ./gradlew check build -x lint allTests --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 fi
