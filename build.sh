eaf6fe60a59651f33545d92eef5db8dc156ffe2b
Rahul Behera <rbehera@gmail.com>
2019-12-08 19:36:45 -0800

    use a daemon for mac since it will already call assemble on the build

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 if [ $TRAVIS_OS_NAME = 'linux' ]; then
   ./gradlew check build allTests --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false
 else
-  ./gradlew check build -x lint allTests --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
+  ./gradlew check build -x lint allTests --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false
 fi
