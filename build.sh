ac8e1335ef5b149015716aad210d6aa6f5507309
Rahul Behera <rbehera@gmail.com>
2019-12-20 20:07:19 -0800

    disable js browser based on the os name

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME = "linux" ]] || [[ $TRAVIS_OS_NAME = "windows" ]]; then
+if [ $TRAVIS_OS_NAME = "linux" ]; then
   ./gradlew check build allTests --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
+elif [ $TRAVIS_OS_NAME = "windows" ]; then
+  ./gradlew check build allTests -x jsBrowserTest --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 else
   ./gradlew check build -x lint allTests --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false
 fi
