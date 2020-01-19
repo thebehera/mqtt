f5ba3927d3a833454763952b7a6959db0497e50d
Rahul Behera <rbehera@gmail.com>
2020-01-19 13:44:52 -0800

    just do a ping response test

--- client/src/androidTest/kotlin/mqtt/client/session/transport/nio/AsyncClientControlPacketTransportIntegrationTests.kt
+++ client/src/androidTest/kotlin/mqtt/client/session/transport/nio/AsyncClientControlPacketTransportIntegrationTests.kt
@@ -65,34 +65,34 @@ class AsyncClientControlPacketTransportIntegrationTests {
         assertNotNull(transport.assignedPort())
         return transport
     }
-
-    @Test
-    fun pingRequestSingleThread() {
-        repeat(runCount) {
-            println("ping request st $it / $runCount")
-            try {
-                runBlocking { pingRequestImpl(singleThreadScope, singleThreadProvider) }
-            } catch (e: Throwable) {
-                println("error from pingRequestSingleThread $it")
-                e.printStackTrace()
-                throw e
-            }
-        }
-    }
-
-    @Test
-    fun pingRequestMultiThread() {
-        repeat(runCount) {
-            println("ping request mt $it / $runCount")
-            try {
-                runBlocking { pingRequestImpl(multiThreadScope, multiThreadProvider) }
-            } catch (e: Throwable) {
-                println("error from pingRequestMultiThread $it")
-                e.printStackTrace()
-                throw e
-            }
-        }
-    }
+//
+//    @Test
+//    fun pingRequestSingleThread() {
+//        repeat(runCount) {
+//            println("ping request st $it / $runCount")
+//            try {
+//                runBlocking { pingRequestImpl(singleThreadScope, singleThreadProvider) }
+//            } catch (e: Throwable) {
+//                println("error from pingRequestSingleThread $it")
+//                e.printStackTrace()
+//                throw e
+//            }
+//        }
+//    }
+//
+//    @Test
+//    fun pingRequestMultiThread() {
+//        repeat(runCount) {
+//            println("ping request mt $it / $runCount")
+//            try {
+//                runBlocking { pingRequestImpl(multiThreadScope, multiThreadProvider) }
+//            } catch (e: Throwable) {
+//                println("error from pingRequestMultiThread $it")
+//                e.printStackTrace()
+//                throw e
+//            }
+//        }
+//    }
 
 
     @Test
