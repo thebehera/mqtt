679da313a31fc8543d9235a149a631653da036bb
Rahul Behera <rbehera@gmail.com>
2020-01-22 23:52:58 -0800

    print more detailed logs for errors

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ $TRAVIS_OS_NAME = "linux" ]; then
-  ./gradlew check build allTests --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
+  ./gradlew --info :client:testReleaseUnitTest --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 elif [ $TRAVIS_OS_NAME = "windows" ]; then
   ./gradlew check build allTests -x jsBrowserTest --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 else
-  ./gradlew check build -x lint allTests --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false
+  ./gradlew --info :client:testReleaseUnitTest --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false
 fi
