cee72a36a73be56c0b797eb9326c1cd950367a46
Rahul Behera <thebehera@fb.com>
2020-01-26 15:44:13 -0800

    disable jvm tests on windows

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ $TRAVIS_OS_NAME = "linux" ]; then
   ./gradlew check build allTests --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 elif [ $TRAVIS_OS_NAME = "windows" ]; then
-  ./gradlew check build allTests -x jsBrowserTest --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
+  ./gradlew check build allTests -x jsBrowserTest -x :transport:jvmTest --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 else
   ./gradlew check build -x lint allTests --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false
 fi
