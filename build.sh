8b0288b9fcd3bd4afcf31249228d0dab17b10ca3
Rahul Behera <thebehera@fb.com>
2020-02-05 23:05:19 -0800

    kill info logs

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $TRAVIS_OS_NAME = "linux" ]; then
-  ./gradlew check build allTests --info --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
+  ./gradlew check build allTests --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 elif [ $TRAVIS_OS_NAME = "windows" ]; then
   ./gradlew check build allTests -x jsBrowserTest -x :transport:jvmTest --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 else
