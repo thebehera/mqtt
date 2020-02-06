cf20af7fa94078aa3e70049db63ec2717f86258f
Rahul Behera <thebehera@fb.com>
2020-02-05 17:00:50 -0800

    wip

---
+++ transport/src/jvmMain/kotlin/mqtt/transport/asyncSocket.kt
@@ -0,0 +1,37 @@
+package mqtt.transport
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import mqtt.transport.nio2.AsyncClientSocket
+import mqtt.transport.nio2.AsyncServerSocket
+import java.nio.ByteBuffer
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+
+@ExperimentalCoroutinesApi
+@ExperimentalTime
+@ExperimentalUnsignedTypes
+actual fun asyncClientSocket(
+    coroutineScope: CoroutineScope,
+    readTimeout: Duration,
+    writeTimeout: Duration
+): ClientToServerSocket<*> {
+    return AsyncClientSocket(coroutineScope, ByteBufferPool, readTimeout, writeTimeout)
+}
+
+object ByteBufferPool : BufferPool<ByteBuffer> {
+    override fun borrow(): ByteBuffer {
+        return ByteBuffer.allocateDirect(8096)
+    }
+}
+
+@ExperimentalCoroutinesApi
+@ExperimentalUnsignedTypes
+@ExperimentalTime
+actual fun asyncServerSocket(
+    coroutineScope: CoroutineScope,
+    readTimeout: Duration,
+    writeTimeout: Duration
+): ServerToClientSocket<*> {
+    return AsyncServerSocket(coroutineScope, ByteBufferPool, readTimeout, writeTimeout)
+}
