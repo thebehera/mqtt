1d59633c81796a3d3696facd087a47172d3459d7
Rahul Behera <thebehera@fb.com>
2020-02-24 22:23:31 -0800

    add allocatenewbuffer again?

---
+++ transport/src/jvmMain/kotlin/mqtt/transport/allocateNewBuffer.kt
@@ -0,0 +1,19 @@
+package mqtt.transport
+
+import mqtt.transport.nio.JvmBuffer
+import java.io.RandomAccessFile
+import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
+import java.nio.file.Files
+
+actual fun allocateNewBuffer(
+    size: UInt,
+    limits: BufferMemoryLimit
+): PlatformBuffer = if (limits.isTooLargeForMemory(size)) {
+    val file = Files.createTempFile(limits.tmpBufferPrefix, null).toFile()
+    val randomAccessFile = RandomAccessFile(file, "rw")
+    val fileMappedBuffer = randomAccessFile.channel!!.map(FileChannel.MapMode.READ_WRITE, 0L, size.toLong())
+    JvmBuffer(fileMappedBuffer, randomAccessFile)
+} else {
+    JvmBuffer(ByteBuffer.allocateDirect(size.toInt()))
+}
