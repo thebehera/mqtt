6bd83610ac46d8092c0a5fd967d8272209f04a2b
Rahul Behera <rbehera@gmail.com>
2020-04-01 20:27:07 -0700

    add info logs to help determine the issue

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ if [ $TRAVIS_OS_NAME = "linux" ]; then
 elif [ $TRAVIS_OS_NAME = "windows" ]; then
   ./gradlew check build allTests -x jsBrowserTest --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false
 else
-  ./gradlew check build -x lint allTests --stacktrace --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false
+  ./gradlew check build -x lint allTests --info --stacktrace --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false
 fi
