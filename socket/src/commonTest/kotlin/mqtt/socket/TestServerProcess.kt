6ae4647d31aeba0541c144a5e87e38379cf80d79
Siddharth Behera <sbehera0@yahoo.com>
2020-04-05 12:34:34 -0500

    client calls from oneServerMultiClient is now asynch.

--- socket/src/commonTest/kotlin/mqtt/socket/TestServerProcess.kt
+++ socket/src/commonTest/kotlin/mqtt/socket/TestServerProcess.kt
@@ -16,10 +16,12 @@ class TestServerProcess : ServerProcessAbs() {
     @ExperimentalTime
     private val writeTimeout = readTimeout
 
+    @ExperimentalUnsignedTypes
     val limits = object : BufferMemoryLimit {
         override fun isTooLargeForMemory(size: UInt) = size > 1_000u
     }
 
+    @ExperimentalUnsignedTypes
     @ExperimentalTime
     override suspend fun serverSideProcess() {
         val rbuffer = allocateNewBuffer(100.toUInt(), limits)
@@ -27,15 +29,15 @@ class TestServerProcess : ServerProcessAbs() {
 
         assertTrue(socket.isOpen(), "Client socket is not open")
 
-        var num : Int = socket.read(rbuffer, readTimeout)
+        socket.read(rbuffer, readTimeout)
 
         var str: String = rbuffer.readMqttUtf8StringNotValidated().toString()
-
+//println("server received msg: $str")
         assertEquals(clientResponse, str.substring(0, clientResponse.length), "Received message is not correct.")
 
         wbuffer.writeUtf8String(str + ":" + name)
 
-        num = socket.write(wbuffer, writeTimeout)
+        socket.write(wbuffer, writeTimeout)
 
     }
 
