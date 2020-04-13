4c4c949251b7cda3ca3805699f65b3110bb4ebf2
Rahul Behera <rbehera@gmail.com>
2020-04-12 19:50:12 -0700

    dont fail on lint

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ if [ $TRAVIS_OS_NAME = "linux" ]; then
 elif [ $TRAVIS_OS_NAME = "windows" ]; then
   ./gradlew check build allTests -x jsBrowserTest --console=plain --max-workers=1 --no-daemon --build-cache -Dkotlin.colors.enabled=false --scan
 else
-  ./gradlew check build -x lint allTests --stacktrace --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false --scan
+  ./gradlew check build -x allTests --stacktrace --console=plain --max-workers=1 --build-cache -Dkotlin.colors.enabled=false --scan
 fi
