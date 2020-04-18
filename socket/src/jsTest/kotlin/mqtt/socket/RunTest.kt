cb12ad3188f7c666e0b74d1b670c3cb87d8ce2ee
Rahul Behera <thebehera@fb.com>
2020-04-18 12:00:26 -0700

    handle the error promise thing

--- socket/src/jsTest/kotlin/mqtt/socket/RunTest.kt
+++ socket/src/jsTest/kotlin/mqtt/socket/RunTest.kt
@@ -4,4 +4,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.promise
 
-actual fun <T> block(body: suspend CoroutineScope.() -> T): dynamic = GlobalScope.promise { body() }
+actual fun <T> block(body: suspend CoroutineScope.() -> T): dynamic = GlobalScope.promise { body() }.catch {
+    if (it !is UnsupportedOperationException) {
+        throw it
+    }
+}
