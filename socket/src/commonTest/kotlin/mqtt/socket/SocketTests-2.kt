f4d5e3116aa64b0f1e1fd8550d46411fe7ba7902
Siddharth Behera <sbehera0@yahoo.com>
2020-05-24 10:17:30 -0500

    chnaged parallel clients.

--- socket/src/commonTest/kotlin/mqtt/socket/SocketTests-2.kt
+++ socket/src/commonTest/kotlin/mqtt/socket/SocketTests-2.kt
@@ -49,7 +49,7 @@ class `SocketTests-2` {
     @Test
     fun oneServerMultiClient() = block {
         var port: UShort = 0u
-        val clientCount = 20
+        val clientCount = 10
         val serverProcess = ServerProcessTest(ServerAction.MQTTSTRING)
         serverProcess.name = "Server-x"
         serverProcess.clientResponse = "Client-"
@@ -75,7 +75,7 @@ class `SocketTests-2` {
     fun twoServerMultiClient() = block {
         var port0: UShort = 0u
         var port1: UShort = 0u
-        val clientCount = 10
+        val clientCount = 5
         val serverProcess = ServerProcessTest(ServerAction.MQTTSTRING)
         serverProcess.name = "Server-x"
         serverProcess.clientResponse = "Client-"
