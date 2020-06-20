3a6ce3c10944e1b30665742db4d27f712c85ccbf
Rahul Behera <thebehera@fb.com>
2020-06-20 10:26:57 -0700

    make sure build passes

--- socket/src/jvmMain/kotlin/mqtt/socket/nio2/AsyncServerToClientSocket.kt
+++ socket/src/jvmMain/kotlin/mqtt/socket/nio2/AsyncServerToClientSocket.kt
@@ -1,11 +1,12 @@
 package mqtt.socket.nio2
 
+import mqtt.buffer.BufferPool
 import java.nio.channels.AsynchronousSocketChannel
 import kotlin.time.ExperimentalTime
 
 @ExperimentalUnsignedTypes
 @ExperimentalTime
-class AsyncServerToClientSocket(asyncSocket: AsynchronousSocketChannel) : AsyncBaseClientSocket() {
+class AsyncServerToClientSocket(asyncSocket: AsynchronousSocketChannel) : AsyncBaseClientSocket(BufferPool()) {
     init {
         this.socket = asyncSocket
     }
