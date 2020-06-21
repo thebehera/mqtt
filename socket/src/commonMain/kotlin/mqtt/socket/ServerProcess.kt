44a7febc58689c012bbc89f25a58bbfca5851d21
Siddharth Behera <sbehera0@yahoo.com>
2020-06-21 15:03:35 -0500

    Defined new methods -- isOpen, read & write

--- socket/src/commonMain/kotlin/mqtt/socket/ServerProcess.kt
+++ socket/src/commonMain/kotlin/mqtt/socket/ServerProcess.kt
@@ -1,6 +1,8 @@
 package mqtt.socket
 
 import kotlinx.coroutines.InternalCoroutinesApi
+import mqtt.buffer.PlatformBuffer
+import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
 interface ServerProcess {
@@ -9,5 +11,10 @@ interface ServerProcess {
     suspend fun newInstance() : ServerProcess
     @ExperimentalTime
     suspend fun startProcessing (socket: ClientSocket)
+    suspend fun isOpen(): Boolean
+    @ExperimentalTime
+    suspend fun read(buffer: PlatformBuffer, timeout: Duration): Int
+    @ExperimentalTime
+    suspend fun write(buffer: PlatformBuffer, timeout: Duration): Int
     suspend fun close()
 }
