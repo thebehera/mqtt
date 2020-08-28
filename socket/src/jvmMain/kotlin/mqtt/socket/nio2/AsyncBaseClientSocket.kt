1dbb7a33f2cc73ce9b6859acd33bee7e687cf1ed
Siddharth Behera <sbehera0@yahoo.com>
2020-08-27 22:32:30 -0500

    enabled ssl-read & ssl-write

--- socket/src/jvmMain/kotlin/mqtt/socket/nio2/AsyncBaseClientSocket.kt
+++ socket/src/jvmMain/kotlin/mqtt/socket/nio2/AsyncBaseClientSocket.kt
@@ -8,26 +8,47 @@ import mqtt.socket.nio.ByteBufferClientSocket
 import mqtt.socket.nio2.util.aRead
 import mqtt.socket.nio2.util.aWrite
 import mqtt.socket.nio2.util.assignedPort
+import mqtt.socket.ssl.SSLProcessor
 import java.nio.channels.AsynchronousSocketChannel
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
 @ExperimentalUnsignedTypes
 @ExperimentalTime
-abstract class AsyncBaseClientSocket(private val pool: BufferPool) :
+abstract class AsyncBaseClientSocket(private val pool: BufferPool, private val ssl: Boolean = false) :
     ByteBufferClientSocket<AsynchronousSocketChannel>() {
     override fun remotePort() = socket?.assignedPort(remote = true)
+    protected lateinit var sslProcess: SSLProcessor
 
     override suspend fun <T> read(timeout: Duration, bufferRead: (PlatformBuffer, Int) -> T): SocketDataRead<T> {
+        var result: T? = null
         var bytesRead = 0
-        val result = pool.borrowSuspend {
-            val byteBuffer = (it as JvmBuffer).byteBuffer
-            bytesRead = socket!!.aRead(byteBuffer, timeout)
-            bufferRead(it, bytesRead)
+
+        if (ssl) {
+            sslProcess.sslRead(timeout) { buf: PlatformBuffer, size: Int ->
+                bytesRead = size
+                result = bufferRead(buf, size)
+            }
+        } else {
+            result = pool.borrowSuspend {
+                val byteBuffer = (it as JvmBuffer).byteBuffer
+                bytesRead = socket!!.aRead(byteBuffer, timeout)
+                bufferRead(it, bytesRead)
+            }
         }
-        return SocketDataRead(result, bytesRead)
+        return SocketDataRead(result!!, bytesRead)
     }
 
-    override suspend fun write(buffer: PlatformBuffer, timeout: Duration) =
-        socket!!.aWrite((buffer as JvmBuffer).byteBuffer, timeout)
+    override suspend fun write(buffer: PlatformBuffer, timeout: Duration): Int {
+        return if (ssl)
+            sslProcess.sslWrite(buffer)
+        else
+            socket!!.aWrite((buffer as JvmBuffer).byteBuffer, timeout)
+    }
+
+    override suspend fun close() {
+        if (ssl)
+            sslProcess.initiateClose()
+        super.close()
+    }
 }
