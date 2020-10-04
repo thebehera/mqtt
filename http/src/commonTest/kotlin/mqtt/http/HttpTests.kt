fc7e785feeb88d5207a325851465efc2aaf5a9fc
Rahul Behera <thebehera@fb.com>
2020-10-04 02:05:19 -0700

    Revert "remove test that could have a confusing name"
    This reverts commit b63f04f3d57e39c9242d08456189ebe99b6d56d9.

--- http/src/commonTest/kotlin/mqtt/http/HttpTests.kt
+++ http/src/commonTest/kotlin/mqtt/http/HttpTests.kt
@@ -1,6 +1,7 @@
 package mqtt.http
 
 import mqtt.http.v1_1.HttpClient
+import mqtt.socket.openClientSocket
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -12,6 +13,25 @@ class HttpTests {
 
     @Test
     fun http() = block {
+        val client = openClientSocket(80u, hostname = "example.com")
+        val request =
+            """
+GET / HTTP/1.1
+Host: example.com
+Connection: close
+
+"""
+        val bytesWritten = client.write(request)
+        assertTrue { bytesWritten > 0 }
+        val response = client.read().result
+        client.close()
+        assertTrue { response.contains("200 OK") }
+        assertTrue { response.contains("HTTP") }
+        assertTrue { response.contains("<html>") }
+    }
+
+    @Test
+    fun http2() = block {
         val response = HttpClient.request(HttpRequest("example.com"))
         assertEquals(response.statusCode, 200)
         assertTrue(response.body!!.contains("<html>"))
