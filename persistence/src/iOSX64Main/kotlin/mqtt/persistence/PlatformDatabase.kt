f8575d9eab251d7feeb462647279b1434f0c0577
Rahul Behera <thebehera@fb.com>
2020-12-29 22:55:02 -0800

    enable native platforms on persistence

--- persistence/src/iOSX64Main/kotlin/mqtt/persistence/PlatformDatabase.kt
+++ persistence/src/iOSX64Main/kotlin/mqtt/persistence/PlatformDatabase.kt
@@ -1,14 +1,11 @@
 package mqtt.persistence
 
 import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 import mqtt.persistence.db.Database
 
 actual fun getPlatformDatabase(name: String, contextProvider: ContextProvider): PlatformDatabase =
     throw UnsupportedOperationException()
 
-
-actual class DatabaseDriverFactory {
-    actual fun createDriver(): SqlDriver {
-        return NativeSqliteDriver(Database.Schema, "test.db")
-    }
-}
+actual fun createDriver(name: String, contextProvider: ContextProvider): SqlDriver =
+    NativeSqliteDriver(Database.Schema, name)
