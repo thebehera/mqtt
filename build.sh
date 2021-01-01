7e292d03b2055b2bc5db3f45420cc970dc277365
Rahul Behera <rbehera@gmail.com>
2020-12-31 22:01:52 -0800

    switch to info

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $TRAVIS_OS_NAME = "linux" ]; then
-  ./gradlew check build allTests --build-cache --scan --stacktrace
+  ./gradlew check build allTests --build-cache --scan --info
 elif [ $TRAVIS_OS_NAME = "windows" ]; then
   ./gradlew check build allTests -x jsBrowserTest --build-cache  --scan
 else
